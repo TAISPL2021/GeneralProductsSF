f2f9496ca435409e4eb361461e3a8017cf087da1
Felix Moreno <felixmorenot18@hotmail.com>
2021-10-20 10:33:58 -0500

    Init Project -> Microservicio Inventario

---
+++ code/back/microservices/ms-inventario/src/main/java/com/topicospl/msinventario/MsInventarioApplication.java
@@ -0,0 +1,13 @@
+package com.topicospl.msinventario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsInventarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsInventarioApplication.class, args);
+	}
+
+}
