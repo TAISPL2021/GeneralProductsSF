3c947e99397da66fd6793d5cda8b7275f64c5519
Felix Moreno <felixmorenot18@hotmail.com>
2021-10-21 09:21:31 -0500

    Inicializacion Proyecto Microservicio componente Catalogo

---
+++ code/back/microservices/ms-catalogo/src/test/java/com/topicospl/mscatalogo/MsCatalogoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.topicospl.mscatalogo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsCatalogoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
