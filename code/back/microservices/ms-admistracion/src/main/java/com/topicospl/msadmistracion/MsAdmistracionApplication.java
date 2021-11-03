a1818d04d6732b44767062237dbfb651d2adbbd9
Felix Rafael Moreno Tabares <felixmorenot18@hotmail.com>
2021-11-03 18:12:19 -0500

    Inclusion Microservicio Administracion

---
+++ code/back/microservices/ms-admistracion/src/main/java/com/topicospl/msadmistracion/MsAdmistracionApplication.java
@@ -0,0 +1,13 @@
+package com.topicospl.msadmistracion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsAdmistracionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsAdmistracionApplication.class, args);
+	}
+
+}
---
+++ code/back/microservices/ms-admistracion/src/test/java/com/topicospl/msadmistracion/MsAdmistracionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.topicospl.msadmistracion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsAdmistracionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
